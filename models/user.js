5959ef34f1c9a4777ca40596e6b53b182e78f87c
trinhamcity1 <trinhamcity1@gmail.com>
2021-03-25 01:04:13 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,22 +4,33 @@ const uuidv1 = require('uuid/v1')
 
 const userSchema = new mongoose.Schema({
     name:{
-        type:String,
+        type: String,
         trim: true,
         required: true,
         maxLength: 32
     },
     email:{
-        type:String,
+        type: String,
         trim: true,
         required: true,
         unique: 32
     },
-    password:{
-        type:String,
-        trim: true,
+    hashed_password:{
+        type: String,
         required: true,
-        unique: 32
     },
-
-})
+    about:{
+        type: String,
+        trim: true,
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    history: {
+        type: Array,
+        default: []
+    }
+}, {timestamps: true}
+);
